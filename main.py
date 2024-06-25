030f84c34927030f94f8512bf9d6cfb87860c7ad
retamp <39591142+retampweb@users.noreply.github.com>
2024-06-25 23:51:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
 from collections import defaultdict
+from operator import itemgetter
 
 # Токен бота
 TOKEN = '7205134080:AAElGKDakWGR3upcttIiDytEt5XVFvPC2s4'
@@ -9,7 +10,7 @@ TOKEN = '7205134080:AAElGKDakWGR3upcttIiDytEt5XVFvPC2s4'
 bot = telebot.TeleBot(TOKEN)
 
 # Словарь для хранения данных пользователей
-users = defaultdict(lambda: {'income': 1, 'referrals': {}, 'staked': 0})
+users = defaultdict(lambda: {'income': 1, 'referrals': {}, 'staked': 0, 'total_referral_income': 0})
 
 # Функция для начала работы с ботом
 @bot.message_handler(commands=['start'])
@@ -21,7 +22,9 @@ def start(message):
     btn1 = types.KeyboardButton("Профиль")
     btn2 = types.KeyboardButton("Реферальная программа")
     btn3 = types.KeyboardButton("Стейкинг")
-    markup.add(btn1, btn2, btn3)
+    btn4 = types.KeyboardButton("Личный кабинет")
+    btn5 = types.KeyboardButton("Таблица лидеров")
+    markup.add(btn1, btn2, btn3, btn4, btn5)
     
     # Приветственное сообщение
     bot.send_message(chat_id, "Добро пожаловать в бот $CONE! Выберите действие:", reply_markup=markup)
@@ -45,6 +48,12 @@ def handle_text(message):
     
     elif text == "Стейкинг":
         process_staking(chat_id)
+    
+    elif text == "Личный кабинет":
+        show_dashboard(chat_id)
+    
+    elif text == "Таблица лидеров":
+        show_leaderboard()
 
 def show_profile(chat_id):
     # Вывод информации о профиле пользователя
@@ -76,6 +85,14 @@ def do_staking(message):
     except ValueError:
         bot.send_message(chat_id, "Некорректное значение. Введите целое число.")
 
+def show_dashboard(chat_id):
+    # Вывод информации о личном кабинете
+    dashboard_info = f"Ваш баланс: {users[chat_id]['income']} $CONE\n"
+    dashboard_info += f"Заблокировано монет: {users[chat_id]['staked']} $CONE\n"
+    dashboard_info += f"Доход от рефералов: {users[chat_id]['total_referral_income']} $CONE\n"
+    dashboard_info += f"Доход от стейкинга в сутки: {users[chat_id]['staked'] * 0.05} $CONE"
+    bot.send_message(chat_id, dashboard_info)
+
 def add_referral(chat_id, ref_code):
     # Добавление реферала к рефереру
     level = 0
@@ -89,11 +106,28 @@ def add_referral(chat_id, ref_code):
         # Увеличение дохода реферера в зависимости от уровня
         income_increase = users[chat_id]['income'] * (0.01 + 0.0017 * level)
         users[parent]['income'] += income_increase
+        users[parent]['total_referral_income'] += income_increase
         
         level += 1
     
     # Приветственное сообщение для реферала
     bot.send_message(chat_id, f"Вы успешно присоединились к реферальной программе! Ваш доход: {users[chat_id]['income']} $CONE в час.")
+
+def show_leaderboard():
+    # Получение списка пользователей, отсортированных по количеству рефералов
+    leaderboard = sorted(users.items(), key=lambda x: sum(len(level) for level in x[1]['referrals'].values()), reverse=True)[:100]
+    
+    # Формирование сообщения с таблицей лидеров
+    leaderboard_msg = "Таблица лидеров:\n\n"
+    leaderboard_msg += "Место | Пользователь | Количество рефералов\n"
+    leaderboard_msg += "-" * 50 + "\n"
+    
+    for i, (user_id, user_data) in enumerate(leaderboard, start=1):
+        referral_count = sum(len(level) for level in user_data['referrals'].values())
+        leaderboard_msg += f"{i}. {user_id} | {referral_count}\n"
+    
+    # Отправка сообщения с таблицей лидеров
+    bot.send_message(chat_id, leaderboard_msg)
 
 # Запуск бота
 bot.polling()
